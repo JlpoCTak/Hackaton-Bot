331ec6311717cc2cc00d2c65c692161b68920e67
MADoMAXs <146708097+MADoMAXs@users.noreply.github.com>
2023-10-25 18:43:38 +0300

    0.0.6

--- database/administrative department.py
+++ database/administrative department.py
@@ -1,21 +1,20 @@
 import sqlite3
 
-connection = sqlite3.connect('database/my.db')
+connection = sqlite3.connect('my_database.db')
 cursor = connection.cursor()
 
 cursor.execute('''
-CREATE TABLE IF NOT EXISTS users (
+CREATE TABLE IF NOT EXISTS administrative_department (
 id INTEGER PRIMARY KEY,
 username TEXT NOT NULL,
-email TEXT NOT NULL
+email TEXT NOT NULL,
 )
 ''')
 
-cursor.execute('INSERT INTO Users (username, email) VALUES (?, ?)', ('1newuser', '1newuser@example.com'))
+cursor.execute('INSERT INTO Users (username, email) VALUES (?, ?)', ('newuser', 'newuser@example.com'))
 
 connection.commit()
 connection.close()
 
 # 1 функция для админ панели  в базу записывается режим отделения, контактная информация, название административного отделения
 # 2 функция для пользователей берет данные из базы
-# C:\Users\Эмиль\OneDrive\Documents\GitHub\Hackaton-Bot
--- telegram/handlers.py
+++ telegram/handlers.py
@@ -25,22 +25,71 @@ router = Router()
 
 @router.message(Command("start"))
 async def start_handler(msg: Message):
+    print(msg.from_user.id)
     connection = sqlite3.connect('database/Users.db')
     cursor = connection.cursor()
     user_id = msg.from_user.id
-    user_check = f'''SELECT * FROM Users WHERE Tg user id = {user_id}'''
-    if (user_check):
-        print(1)
+    user_check_admin = f'''SELECT EXISTS(SELECT Fio FROM Admin WHERE Tg_users_ID = ?)'''
 
-    builder = InlineKeyboardBuilder()
-    builder.add(types.InlineKeyboardButton(
-        text="Админ-панель",
-        callback_data="Test")
-    )
-    await msg.answer(
-        f'Привет,Админ {msg.from_user.full_name}, нажми на кнопку, чтобы вйоти в админ-панель',
-        reply_markup=builder.as_markup()
-    )
+    status = 0
+    user_check_teacher = f'''SELECT EXISTS(SELECT Fio FROM Teacher WHERE Tg_users_ID = ?)'''
+    user_check_student = f'''SELECT EXISTS(SELECT Fio FROM Student WHERE Tg_users_ID = ?)'''
+    check_admin = cursor.execute(user_check_admin, (user_id,))
+
+
+    for check1 in check_admin:
+        if list(check1)[0] == 1:
+            status = 'admin'
+        break
+    check_teacher = cursor.execute(user_check_teacher, (user_id,))
+    for check2 in check_teacher:
+        if list(check2)[0] == 1:
+            status = 'teacher'
+        break
+    check_student = cursor.execute(user_check_student, (user_id,))
+    for check3 in check_student:
+        if list(check3)[0] == 1:
+            status = 'student'
+        break
+
+    print(status)
+
+    if status == 'admin':
+        builder = InlineKeyboardBuilder()
+        builder.add(types.InlineKeyboardButton(
+            text="Админ-панель",
+            callback_data="admin_panel")
+        )
+        await msg.answer(
+            f'Привет,Админ {msg.from_user.full_name}, нажми на кнопку, чтобы войти в админ-панель',
+            reply_markup=builder.as_markup()
+            )
+
+    elif status == 'teacher':
+        builder = InlineKeyboardBuilder()
+        builder.add(types.InlineKeyboardButton(
+            text="кнопка преподавателя",
+            callback_data="func_1")
+        )
+        await msg.answer(
+            f'Привет,преподаватель {msg.from_user.full_name}, нажми на кнопку с функцией, которая тебе нужна',
+            reply_markup=builder.as_markup()
+        )
+    elif status == 'student':
+        builder = InlineKeyboardBuilder()
+        builder.add(types.InlineKeyboardButton(
+            text="кнопка студента",
+            callback_data="Test")
+        )
+        await msg.answer(
+            f'Привет,студент {msg.from_user.full_name}, нажми на кнопку с функцией, которая тебе нужна',
+            reply_markup=builder.as_markup()
+        )
+
+
+
+
+
 @router.message(Command("Logging"))
 async def Logging_or_register(msg: types.Message):
     await msg.answer("Хотите-ли ")
