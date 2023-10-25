8a024f9a810d33da71f1b074bbc2151ab8c67c02
M0mong0 <tisenkoemil@gmail.com>
2023-10-25 17:22:56 +0300

    123

--- database/administrative department.py
+++ database/administrative department.py
@@ -3,19 +3,27 @@ import sqlite3
 connection = sqlite3.connect('database/my.db')
 cursor = connection.cursor()
 
-cursor.execute('''
-CREATE TABLE IF NOT EXISTS users (
-id INTEGER PRIMARY KEY,
-username TEXT NOT NULL,
-email TEXT NOT NULL
-)
-''')
+def admin_menu():
+    a = input()
+    print("Меню администратора")
+    if (a == 1):
+        adding_a_department()
+def adding_a_department():
+    id = input()
+    name = input()
+    email = input()
+    department_mode = input()
+    cursor.execute('INSERT INTO users (id, name, email, department_mode) VALUES (?, ?, ?, ?)', (id, name, email, department_mode))
+def student():
+    cursor.execute('SELECT * FROM Users')
+    users = cursor.fetchall()
 
-cursor.execute('INSERT INTO Users (username, email) VALUES (?, ?)', ('1newuser', '1newuser@example.com'))
-
+    for user in users:
+        print(user)
+adding_a_department()
 connection.commit()
 connection.close()
 
-# 1 функция для админ панели  в базу записывается режим отделения, контактная информация, название административного отделения
+# 1 функция для админ панели в базу записывается режим отделения, контактная информация, название административного отделения
 # 2 функция для пользователей берет данные из базы
 # C:\Users\Эмиль\OneDrive\Documents\GitHub\Hackaton-Bot
