424964de8f96e5ab7a3afb5fdcd906a4ddbb1f22
MADoMAXs <146708097+MADoMAXs@users.noreply.github.com>
2023-10-26 10:40:54 +0300

    don't work

--- telegram/homework.py
+++ telegram/homework.py
@@ -2,22 +2,24 @@
 
 import sqlite3
 
+
 def prepod():
-    connection = sqlite3.connect('database/Users.db')
+    connection = sqlite3.connect('C:\\Users\\Sokol\\Documents\\GitHub\\Hackaton-Bot\\database\\Users.db')
     cursor = connection.cursor()
 
     # id = input("Введите айди: ")
     weak_day = input("Введите день недели: ")
     data = input("Введите дату: ")
-    group = input("Введите группу: ")
+    student_class = input("Введите группу: ")
     teacher = input("Введите учителя: ")
     lesson = input("Введите урок: ")
     href = 'database/homework/dz.docx'
 
+    connection.execute(
+        "INSERT INTO homework VALUES (id = ?, weak_day = ?, data = ?, student_class = ?, teacher = ?, lesson = ?, href = ?)",
+        (id, weak_day, data, student_class, teacher, lesson, href))
 
-    # connection.execute("INSERT INTO homework VALUES (id = ?, weak_day = ?, data = ?, group = ?, teacher = ?, lesson = ?, href = ?)", (id, weak_day, data, group, teacher, lesson, href))
-
-    cursor.execute("INSERT INTO homework VALUES (?, ?, ?, ?, ?, ?)", (weak_day, data, group, teacher, lesson, href))
+    cursor.execute("INSERT INTO homework VALUES (?, ?, ?, ?, ?, ?)", (weak_day, data, student_class, teacher, lesson, href))
 
     connection.commit()
     print("Задание добавлено успешно!")
@@ -25,7 +27,10 @@ def prepod():
     connection.commit()
     connection.close()
     # выбирает день урок и прикрепляет файл для скачивания
+
+
 prepod()
+
 def student():
     connection = sqlite3.connect('Users.db')
     cursor = connection.cursor()
@@ -43,5 +48,5 @@ def student():
     else:
         print("Задание не найдено.")
     connection.close()
-#выбирает день урок и если есть прикрепленный файл то имеет возможность скачать его
+# выбирает день урок и если есть прикрепленный файл то имеет возможность скачать его
 # prepod()
