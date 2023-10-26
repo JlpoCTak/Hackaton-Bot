36915d032f3ce186a3fba359586558b8103c82ce
M0mong0 <tisenkoemil@gmail.com>
2023-10-26 14:35:14 +0300

    0.1.4

--- telegram/homework.py
+++ telegram/homework.py
@@ -1,84 +1,61 @@
 # id den nedel data group prepod predmet ssilka dz
 # выбирает день урок и прикрепляет файл для скачивания
-#выбирает день урок и если есть прикрепленный файл то имеет возможность скачать его
+# выбирает день урок и если есть прикрепленный файл то имеет возможность скачать его
 
 import sqlite3
 
-
 def prepod():
-<<<<<<< HEAD
-=======
-    connection = sqlite3.connect('C:\\Users\\Sokol\\Documents\\GitHub\\Hackaton-Bot\\database\\Users.db')
-    cursor = connection.cursor()
+    conn = sqlite3.connect("database/Users.db")
+    cursor = conn.cursor()
+    conn = sqlite3.connect("database/Users.db")
+    cursor = conn.cursor()
+    cursor.execute("SELECT weekday FROM schedule")
+    weekday = cursor.fetchall()
+    conn = sqlite3.connect("database/Users.db")
+    cursor = conn.cursor()
+    cursor.execute("SELECT group_name FROM schedule")
+    group_name = cursor.fetchall()
+    conn = sqlite3.connect("database/Users.db")
+    cursor = conn.cursor()
+    cursor.execute("SELECT teacher FROM schedule")
+    teacher = cursor.fetchall()
+    lesson_name = input("Введите название предмета: ")
+    data1 = input("Введите дату: ")
+    file_path = input("Введите домашнее задание")
 
-    # id = input("Введите айди: ")
-    weak_day = input("Введите день недели: ")
-    data = input("Введите дату: ")
-    student_class = input("Введите группу: ")
-    teacher = input("Введите учителя: ")
-    lesson = input("Введите урок: ")
-    href = 'database/homework/dz.docx'
+    # print(group_name)
+    # print(lesson_name)
+    # print(teacher)
 
-    connection.execute(
-        "INSERT INTO homework VALUES (id = ?, weak_day = ?, data = ?, student_class = ?, teacher = ?, lesson = ?, href = ?)",
-        (id, weak_day, data, student_class, teacher, lesson, href))
+    conn = sqlite3.connect("database/Users.db")
+    cursor = conn.cursor()
 
-    cursor.execute("INSERT INTO homework VALUES (?, ?, ?, ?, ?, ?)", (weak_day, data, student_class, teacher, lesson, href))
+    cursor.execute("INSERT INTO homework (weekday, group_name, teacher, lesson_name, data, file_path) VALUES (?, ?, ?, ?, ?, ?)",
+                   (weekday, group_name, teacher, lesson_name, data1,  file_path))
 
-    connection.commit()
-    print("Задание добавлено успешно!")
-
-    connection.commit()
-    connection.close()
+    cursor.execute("SELECT * FROM Schedule WHERE data = ? and lesson_name = ?", data1, lesson_name)
+    conn.commit()
+    conn.close()
     # выбирает день урок и прикрепляет файл для скачивания
-
 
 prepod()
 
 def student():
-    connection = sqlite3.connect('Users.db')
-    cursor = connection.cursor()
-
->>>>>>> 83d73640847988f26e1374a0693cc13697224355
-    day = input("Введите день недели: ")
-    date = input("Введите дату: ")
-    group = input("Введите группу: ")
-    teacher = input("Введите имя учителя: ")
-    lesson = input("Введите название урока: ")
-    file_path = input("Введите домашнее задание: ")
+    weekday = input("Введите день недели: ")
+    lesson_name = input("Введите урок: ")
 
     conn = sqlite3.connect("database/Users.db")
     cursor = conn.cursor()
 
-    # Use a parameterized query
-    cursor.execute("INSERT INTO homework (day, date, group_name, teacher, lesson, file_path) VALUES (?, ?, ?, ?, ?, ?)",
-                   (day, date, group, teacher, lesson, file_path))
-
-    conn.commit()
-    conn.close()
-
-def student():
-    day = input("Введите день недели: ")
-    lesson = input("Введите урон: ")
-
-    conn = sqlite3.connect("database/Users.db")
-    cursor = conn.cursor()
-
-    # Use a parameterized query
-    cursor.execute("SELECT file_path FROM homework WHERE day = ? AND lesson = ?", (day, lesson))
+    cursor.execute("SELECT file_path FROM homework WHERE weekday = ? AND lesson_name = ?", (weekday, lesson_name))
     result = cursor.fetchone()
 
     if result:
         print("Дамашнее задание можно скачать здесь:", result[0])
     else:
-<<<<<<< HEAD
+
         print("По этим урокам в этот день не найдено никакого домашнего задания")
 
     conn.close()
-student()
-=======
-        print("Задание не найдено.")
-    connection.close()
+    student()
 # выбирает день урок и если есть прикрепленный файл то имеет возможность скачать его
-# prepod()
->>>>>>> 83d73640847988f26e1374a0693cc13697224355
